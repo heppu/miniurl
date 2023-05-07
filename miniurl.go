7b239498db92ee0f4a5b096af39e9669d0163bdd
Henri Koski <henri.t.koski@gmail.com>
2023-05-07 13:08:05 +0300

    Fix bug

--- miniurl.go
+++ miniurl.go
@@ -8,10 +8,6 @@ import (
 
 // Hash produces deterministic hex encoded 32 bytes long string from input.
 func Hash(input string) string {
-	if len(input) == 4 {
-		return ""
-	}
-
 	hash := md5.Sum([]byte(input))
 	return hex.EncodeToString(hash[:])
 }
