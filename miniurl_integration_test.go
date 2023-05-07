c937169823013e95688fd6fdb9bef4c8bc2ed60f
Henri Koski <henri.t.koski@gmail.com>
2023-05-08 01:26:49 +0300

    Add integration test

---
+++ miniurl_integration_test.go
@@ -0,0 +1,53 @@
+//go:build integration
+
+package miniurl_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/heppu/miniurl"
+	"github.com/heppu/miniurl/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegration(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.URL.Path)
+	}))
+	t.Cleanup(srv.Close)
+
+	errCh := make(chan error, 1)
+	go func() {
+		err := miniurl.Run()
+		assert.NoError(t, err)
+		errCh <- err
+	}()
+	time.Sleep(1 * time.Second)
+
+	testUrl := fmt.Sprintf(`{"url": "%s"}`, srv.URL+"/testpath")
+	resp, err := http.Post("http://127.0.0.1:8080/api/v1/url", "application/json", strings.NewReader(testUrl))
+	require.NoError(t, err)
+
+	var v api.AddUrlResp
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	require.NoError(t, err)
+
+	resp, err = http.Get("http://127.0.0.1:8080/" + v.Hash)
+	require.NoError(t, err)
+	data, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	assert.Equal(t, "/testpath", string(data))
+
+	syscall.Kill(os.Getpid(), syscall.SIGINT)
+	require.NoError(t, <-errCh)
+}
