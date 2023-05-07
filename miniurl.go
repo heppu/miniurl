5ecd236a296b6dc12ba691d85e01c99bc8213125
Henri Koski <henri.t.koski@gmail.com>
2023-05-07 12:53:28 +0300

    Add bug in hash

--- miniurl.go
+++ miniurl.go
@@ -8,6 +8,10 @@ import (
 
 // Hash input into hex encoded 32 bytes long string.
 func Hash(input string) string {
+	if len(input) == 4 {
+		return ""
+	}
+
 	hash := md5.Sum([]byte(input))
 	return hex.EncodeToString(hash[:])
 }
