b86c2621eab61cd26eb4a8771f44b54324469f80
Henri Koski <henri.t.koski@gmail.com>
2023-05-07 12:30:17 +0300

    Init module

---
+++ miniurl.go
@@ -0,0 +1,13 @@
+// Package miniurl provides required application logic for url shortening app.
+package miniurl
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// Hash input into hex encoded 32 bytes long string.
+func Hash(input string) string {
+	hash := md5.Sum([]byte(input))
+	return hex.EncodeToString(hash[:])
+}
