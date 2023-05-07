3eaa31e225d09c2df77525479c758b18d87348b2
Henri Koski <henri.t.koski@gmail.com>
2023-05-07 22:28:38 +0300

    Add runnable app

--- miniurl.go
+++ miniurl.go
@@ -4,9 +4,63 @@ package miniurl
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/heppu/miniurl/api"
+	"github.com/heppu/miniurl/storage/mem"
+	"golang.org/x/exp/slog"
 )
 
+type Storage interface {
+	AddUrl(url, hash string) error
+	GetUrl(hash string) (url string, err error)
+}
+
+type App struct {
+	storage Storage
+}
+
+func Run() error {
+	addr := os.Getenv("LISTEN_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	app := &App{storage: mem.NewStorage()}
+	srv := api.NewServer(addr, app)
+
+	slog.Info("Starting app", slog.String("LISTEN_ADDR", addr))
+	return srv.Start()
+}
+
+func (a *App) AddUrl(url string) (string, error) {
+	seed := url
+	for i := 0; i < 100; i++ {
+		hash := Hash(seed)[0:5]
+		err := a.storage.AddUrl(url, hash)
+		switch {
+		case err == nil:
+			return hash, nil
+		case !errors.Is(err, mem.ErrHashCollision):
+			return "", fmt.Errorf("failed to store url: %w", err)
+		default:
+			seed = hash
+		}
+	}
+
+	err := fmt.Errorf("unable to get new hash for url: %s", url)
+	slog.Error(err.Error())
+	return "", err
+}
+
 // Hash produces deterministic hex encoded 32 bytes long string from input.
+func (a *App) GetUrl(hash string) (string, error) {
+	return a.storage.GetUrl(hash)
+}
+
+// Hash input into hex encoded 32 bytes long string.
 func Hash(input string) string {
 	hash := md5.Sum([]byte(input))
 	return hex.EncodeToString(hash[:])
