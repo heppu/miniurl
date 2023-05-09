fe966057cf2104e7df1aab4c9c2a5a7132f5968b
Henri Koski <henri.t.koski@gmail.com>
2023-05-09 21:19:08 +0300

    Use postgres as storage

--- miniurl_integration_test.go
+++ miniurl_integration_test.go
@@ -3,6 +3,7 @@
 package miniurl_test
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -18,9 +19,13 @@ import (
 	"github.com/heppu/miniurl/api"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go/modules/compose"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestIntegration(t *testing.T) {
+	setupTestEnv(t)
+
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, r.URL.Path)
 	}))
@@ -50,4 +55,17 @@ func TestIntegration(t *testing.T) {
 
 	syscall.Kill(os.Getpid(), syscall.SIGINT)
 	require.NoError(t, <-errCh)
+}
+
+func setupTestEnv(t *testing.T) {
+	stack, err := compose.NewDockerCompose("docker-compose.yaml")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		assert.NoError(t, stack.Down(context.Background(), compose.RemoveVolumes(true), compose.RemoveOrphans(true), compose.RemoveImagesLocal))
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+	require.NoError(t, stack.WaitForService("db", wait.ForHealthCheck()).Up(ctx, compose.Wait(true)))
 }
