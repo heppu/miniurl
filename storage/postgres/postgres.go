89a5fc5c6d872546d1654baf7c2d74fbf46fb80c
Henri Koski <henri.t.koski@gmail.com>
2023-05-09 12:11:10 +0300

    Add postgres storage

---
+++ storage/postgres/postgres.go
@@ -0,0 +1,79 @@
+package postgres
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/go-gorp/gorp/v3"
+	"github.com/lib/pq"
+)
+
+var (
+	ErrHashCollision = errors.New("hash collides with different url")
+	ErrUrlNotFound   = errors.New("url not found")
+)
+
+type record struct {
+	Hash string `db:"hash, primarykey"`
+	Url  string `db:"url"`
+}
+
+type Storage struct {
+	db *gorp.DbMap
+}
+
+func NewStorage(connStr string) (*Storage, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open sql connection: %w", err)
+	}
+
+	dbmap := &gorp.DbMap{
+		Db:      db,
+		Dialect: gorp.PostgresDialect{},
+	}
+
+	dbmap.AddTableWithName(record{}, "records")
+	if err := dbmap.CreateTablesIfNotExists(); err != nil {
+		return nil, fmt.Errorf("failed to create missing tables: %w", err)
+	}
+
+	return &Storage{db: dbmap}, nil
+}
+
+func (s *Storage) AddUrl(url, hash string) error {
+	err := s.db.Insert(&record{Hash: hash, Url: url})
+	if !violatesUniqueConstrain(err) {
+		return err
+	}
+
+	oldUrl, err := s.GetUrl(hash)
+	if err != nil {
+		return err
+	}
+
+	if oldUrl != url {
+		return ErrHashCollision
+	}
+
+	return nil
+}
+
+func (s *Storage) GetUrl(hash string) (string, error) {
+	url, err := s.db.SelectStr("SELECT url FROM records WHERE hash = $1", hash)
+	if err != nil {
+		return "", err
+	}
+	if url == "" {
+		return "", ErrUrlNotFound
+	}
+
+	return url, nil
+}
+
+func violatesUniqueConstrain(err error) bool {
+	const uniqErrCode = "23505"
+	var pgErr *pq.Error
+	return errors.As(err, &pgErr) && string(pgErr.Code) == uniqErrCode
+}
