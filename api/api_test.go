a38516e7a7a5b6e0b6372dc1a571305690ae2f31
Henri Koski <henri.t.koski@gmail.com>
2023-05-10 01:25:31 +0300

    Add API stub

---
+++ api/api_test.go
@@ -0,0 +1,35 @@
+package api_test
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/heppu/miniurl/api"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAPI_AddUrl(t *testing.T) {
+	t.Skip()
+	const (
+		payload            = `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE"}`
+		expectedBody       = `{"url": "https://github.com/gourses/miniurl/blob/main/LICENSE", "hash": "testvalue"}`
+		expectedStatusCode = http.StatusOK
+	)
+
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/url", strings.NewReader(payload))
+	rr := httptest.NewRecorder()
+
+	r := httprouter.New()
+	api.Bind(r, nil)
+	r.ServeHTTP(rr, req)
+
+	assert.Equal(t, expectedStatusCode, rr.Result().StatusCode)
+	body, err := io.ReadAll(rr.Result().Body)
+	require.NoError(t, err)
+	assert.JSONEq(t, expectedBody, string(body))
+}
