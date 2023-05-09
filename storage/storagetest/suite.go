ff4c7ab1ea96cfbdc5dd2b4d037983eb6167f8a8
Henri Koski <henri.t.koski@gmail.com>
2023-05-09 13:00:11 +0300

    Add storage test suite

--- storage/storagetest/suite.go
+++ storage/storagetest/suite.go
@@ -1,22 +1,48 @@
-package mem_test
+package storagetest
 
 import (
+	"reflect"
+	"runtime"
+	"strings"
 	"sync"
 	"testing"
 
 	"github.com/heppu/miniurl/storage"
-	"github.com/heppu/miniurl/storage/mem"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestStorage_AddAndGet(t *testing.T) {
+type Storage interface {
+	AddUrl(url, hash string) error
+	GetUrl(hash string) (url string, err error)
+}
+
+type SuiteFn func(t *testing.T, s Storage)
+
+func RunSuite(t *testing.T, s Storage) {
+	tests := []SuiteFn{
+		AddAndGet,
+		NotFound,
+		HashCollisionWithDifferentUrl,
+		HashCollisionWithSameUrl,
+		Parallel_AddAndGet,
+	}
+
+	for _, fn := range tests {
+		parts := strings.Split(runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name(), ".")
+		name := parts[len(parts)-1]
+		t.Run(name, func(t *testing.T) {
+			fn(t, s)
+		})
+	}
+}
+
+func AddAndGet(t *testing.T, s Storage) {
 	const (
 		url  = "https://google.com"
-		hash = "hash"
+		hash = "hash-1"
 	)
 
-	s := mem.NewStorage()
 	err := s.AddUrl(url, hash)
 	require.NoError(t, err)
 
@@ -25,48 +51,44 @@ func TestStorage_AddAndGet(t *testing.T) {
 	assert.Equal(t, url, gotUrl)
 }
 
-func TestStorage_NotFound(t *testing.T) {
-	s := mem.NewStorage()
+func NotFound(t *testing.T, s Storage) {
 	url, err := s.GetUrl("hash")
 	assert.ErrorIs(t, err, storage.ErrUrlNotFound)
 	assert.Empty(t, url)
 }
 
-func TestStorage_HashCollisionWithDifferentUrl(t *testing.T) {
+func HashCollisionWithDifferentUrl(t *testing.T, s Storage) {
 	const (
 		url1 = "https://google.com"
 		url2 = "https://bing.com"
-		hash = "hash"
+		hash = "hash-2"
 	)
 
-	s := mem.NewStorage()
 	err := s.AddUrl(url1, hash)
 	require.NoError(t, err)
 	err = s.AddUrl(url2, hash)
 	require.ErrorIs(t, err, storage.ErrHashCollision)
 }
 
-func TestStorage_HashCollisionWithSameUrl(t *testing.T) {
+func HashCollisionWithSameUrl(t *testing.T, s Storage) {
 	const (
 		url  = "https://google.com"
-		hash = "hash"
+		hash = "hash-3"
 	)
 
-	s := mem.NewStorage()
 	err := s.AddUrl(url, hash)
 	require.NoError(t, err)
 	err = s.AddUrl(url, hash)
 	require.NoError(t, err)
 }
 
-func TestStorage_Parallel_AddAndGet(t *testing.T) {
+func Parallel_AddAndGet(t *testing.T, s Storage) {
 	data := map[string]string{
 		"hash-a": "url-a",
 		"hash-b": "url-b",
 		"hash-c": "url-c",
 	}
 
-	s := mem.NewStorage()
 	wg := sync.WaitGroup{}
 	for hash, url := range data {
 		for i := 0; i < 10; i++ {
