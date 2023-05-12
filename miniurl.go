1f782fa1297803967176ea03a8ac368533bfcd2c
Henri Koski <henri.t.koski@gmail.com>
2023-05-12 08:52:00 +0300

    Add Run for miniurl

--- miniurl.go
+++ miniurl.go
@@ -4,7 +4,22 @@ package miniurl
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"os"
+
+	"github.com/heppu/miniurl/api"
+	"golang.org/x/exp/slog"
 )
+
+func Run() error {
+	addr := os.Getenv("LISTEN_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	slog.Info("starting server", slog.String("LISTEN_ADDR", addr))
+	srv := api.NewServer(addr, nil)
+	return srv.Start()
+}
 
 // Hash generates 32 bytes long deterministic string.
 func Hash(input string) string {
